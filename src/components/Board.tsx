c3f6975ad69119133c701c0f25c494dd558e90d8
advait-deshmukh <advait.deshmukh@ringcentral.com>
2025-02-08 13:55:58 +0530

    Create app folder structure

---
+++ src/components/Board.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Slot from "./Slot";
+
+const Board = () => {
+  return (
+    <div>
+      Board
+      <Slot />
+    </div>
+  );
+};
+
+export default Board;
---
+++ src/components/Slot.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Slot = () => {
+  return <div>Slot</div>;
+};
+
+export default Slot;
